41df048716516dcf3f9b1704efa7133c15639b1a
asceth <machinist@asceth.com>
2009-07-17 17:32:46 -0400

    require files

--- lib/scruffy/layers.rb
+++ lib/scruffy/layers.rb
@@ -10,7 +10,7 @@ module Scruffy::Layers
   # Should be raised whenever a predictable error during rendering occurs,
   # particularly if you do not want to terminate the graph rendering process.
   class RenderError < StandardError; end
-  
+
 end
 
 require 'scruffy/layers/base'
@@ -18,6 +18,7 @@ require 'scruffy/layers/area'
 require 'scruffy/layers/all_smiles'
 require 'scruffy/layers/bar'
 require 'scruffy/layers/line'
+require 'scruffy/layers/flowing_line'
 require 'scruffy/layers/average'
 require 'scruffy/layers/stacked'
 require 'scruffy/layers/pie'
