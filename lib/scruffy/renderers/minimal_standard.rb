cf6579e6a5c2ff8b64f4c4b47f09adfbbaf86b01
asceth <machinist@asceth.com>
2009-08-21 19:13:59 -0400

    changes to layout of minimal standard

--- lib/scruffy/renderers/minimal_standard.rb
+++ lib/scruffy/renderers/minimal_standard.rb
@@ -5,14 +5,14 @@ module Scruffy::Renderers
       super do |components|
         components << Scruffy::Components::Viewport.new(:view, :position => [0, 0], :size => [100, 100]) do |graph|
 
-          graph << Scruffy::Components::ValueMarkers.new(:values, :position => [0, 4], :size => [4, 88])
+#          graph << Scruffy::Components::ValueMarkers.new(:values, :position => [0, 4], :size => [4, 88])
 
-          graph << Scruffy::Components::Grid.new(:grid, :position => [8, 4], :size => [84, 88])
-          graph << Scruffy::Components::Graphs.new(:graphs, :position => [8, 4], :size => [84, 88])
+#          graph << Scruffy::Components::Grid.new(:grid, :position => [8, 4], :size => [84, 88])
+          graph << Scruffy::Components::Graphs.new(:graphs, :position => [0, 5], :size => [100, 95])
 
-          graph << Scruffy::Components::ValueMarkers.new(:values, :position => [94, 4], :size => [4, 88])
+#          graph << Scruffy::Components::ValueMarkers.new(:values, :position => [94, 4], :size => [4, 88])
 
-          graph << Scruffy::Components::DataMarkers.new(:labels, :position => [8, 90], :size => [84, 6])
+#          graph << Scruffy::Components::DataMarkers.new(:labels, :position => [8, 90], :size => [84, 6])
         end
       end
     end
