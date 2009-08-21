c1f000fa3f1cfc9f172aa844b8f98e0ad2d288fd
asceth <machinist@asceth.com>
2009-08-21 19:14:19 -0400

    add more options for theme font sizes

--- lib/scruffy/themes.rb
+++ lib/scruffy/themes.rb
@@ -6,7 +6,7 @@
 # Scruffy Themes allow you to alter the colors and appearances of
 # your graph.
 module Scruffy::Themes
-  
+
   # ==Scruffy::Themes::Base
   #
   # Author:: Brasten Sager & A.J. Ostman
@@ -21,6 +21,9 @@ module Scruffy::Themes
     attr_accessor :colors         # Array of colors for data graphs
     attr_accessor :marker         # Marker color for grid lines, values, etc.
     attr_accessor :font_family    # Font family: Not really supported.  Maybe in the future.
+    attr_accessor :legend_font_size
+    attr_accessor :data_marker_font_size
+    attr_accessor :value_marker_font_size
 
     # Returns a new Scruffy::Themes::Base object.
     #
@@ -29,33 +32,36 @@ module Scruffy::Themes
     # colors:: an array of color values to use for graphs.
     # marker:: color used for grid lines, values, data points, etc.
     # font_family:: in general, allows you to change the font used in the graph.
-    #               This is not yet supported in most graph elements, 
+    #               This is not yet supported in most graph elements,
     #               and may be deprecated soon anyway.
     def initialize(descriptor)
       self.background = descriptor[:background]
       self.colors     = descriptor[:colors]
       self.marker     = descriptor[:marker]
       self.font_family = descriptor[:font_family]
+      self.legend_font_size = descriptor[:legend_font_size]
+      self.data_marker_font_size = descriptor[:data_marker_font_size]
+      self.value_marker_font_size = descriptor[:value_marker_font_size]
     end
-    
+
     # Returns the next available color in the color array.
     def next_color
       @previous_color = 0 if @previous_color.nil?
       @previous_color += 1
-              
+
       self.colors[(@previous_color-1) % self.colors.size]
     end
 
     # todo: Implement darken function.
     def darken(color, shift=20); end
-    
+
     # todo: Implement lighten function.
     def lighten(color, shift=20); end
-    
-  end
-  
 
-  
+  end
+
+
+
   # A basic default theme
   # Based on http://www.wellstyled.com/tools/colorscheme2/index-en.html?tetrad;50;0;255;1;-1;1;-0.6;0.1;1;0.6;1;1;-1;1;-0.6;0.1;1;0.6;1;1;-1;1;-0.6;0.1;1;0.6;1;1;-1;1;-0.6;0.1;1;0.6;1;0
   class Standard < Base
@@ -65,18 +71,18 @@ module Scruffy::Themes
               :marker => '#999999',
               :colors => %w(#1919B3 #FFB200 #FFFF00 #660099 #E9E9FF #FFF7E6 #FFFFE6 #F7E6FF #0F0F6B #996B00 #999900 #3D005C)
             })
-    
+
     end
   end
-  
+
   # Keynote theme, based on Apple's Keynote presentation software.
   #
   # Color values used from Gruff's default theme.
   class Keynote < Base
     def initialize
-      super({  
+      super({
               :background => [:black, '#4A465A'],
-              :marker => :white, 
+              :marker => :white,
               :colors => %w(#6886B4 #FDD84E #72AE6E #D1695E #8A6EAF #EFAA43 white)
              })
     end
@@ -90,10 +96,10 @@ module Scruffy::Themes
               :marker => :white,
               :colors => %w(#DD3300 #66AABB #225533 #992200)
             })
-      
+
     end
   end
-  
+
   # Based on the color scheme used by almost every Ruby blogger.
   class RubyBlog < Base
     def initialize
@@ -104,7 +110,7 @@ module Scruffy::Themes
             })
     end
   end
-  
+
   # Inspired by http://www.colorschemer.com/schemes/
   class Apples < Base
     def initialize
@@ -115,8 +121,8 @@ module Scruffy::Themes
             })
     end
   end
-  
-  # Inspired by http://www.colorschemer.com/schemes/  
+
+  # Inspired by http://www.colorschemer.com/schemes/
   class CareBears < Base
     def initialize
       super({
@@ -128,9 +134,9 @@ module Scruffy::Themes
               :colors => %w(#FFBBBB #00CC33 #7788BB #EEAA44 #FFDD11 #44BBDD #DD6677)
             })
     end
-  end  
-  
-  
+  end
+
+
   # Inspired by http://www.colorschemer.com/schemes/
   class Vitamins < Base
     def initialize
@@ -141,7 +147,7 @@ module Scruffy::Themes
             })
     end
   end
-  
+
   # Inspired by http://www.colorschemer.com/schemes/
   class Tulips < Base
     def initialize
@@ -152,5 +158,5 @@ module Scruffy::Themes
             })
     end
   end
-  
+
 end
